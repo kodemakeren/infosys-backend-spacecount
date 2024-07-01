f28a2fe83801f49f79ce571dff8eec6bd1cfddbf
Anders Syvertsen <syvertsen.anders@gmail.com>
2024-07-01 13:18:47 +0200

    Initialisert Spring boot prosjekt med Spring initializr

---
+++ infosys-backend-spacecount/src/main/java/no/infossys/spacecount/SpacecountApplication.java
@@ -0,0 +1,13 @@
+package no.infossys.spacecount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpacecountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpacecountApplication.class, args);
+	}
+
+}
---
+++ infosys-backend-spacecount/src/test/java/no/infossys/spacecount/SpacecountApplicationTests.java
@@ -0,0 +1,13 @@
+package no.infossys.spacecount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpacecountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
